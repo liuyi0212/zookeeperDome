1d5107fe95bdd4b6b823ac9ee8d92240b706e4ab
liuy <liuy@liuydeMacBook-Pro.local>
2018-06-08 17:07:13 +0800

    first commit

---
+++ src/test/java/com/hakuna/zookeeperDome/ZookeeperDomeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hakuna.zookeeperDome;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZookeeperDomeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
